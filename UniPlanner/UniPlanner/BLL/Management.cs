744cc071eeaacfd548ef67603291a6b1e41dc87b
yuriy-muzychuk <yuriy.muzychuk@gmail.com>
2015-11-18 18:57:05 +0200

    ideas for dal

---
+++ UniPlanner/UniPlanner/BLL/Management.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniPlanner.DTO;
+
+namespace UniPlanner.BLL
+{
+    class Management
+    {
+        public IEnumerable<LessonDTO> GetLessonsForStudentOnDay(Guid studentId, DayOfWeek day)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UniPlanner/UniPlanner/DAL/LessonDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniPlanner.Entities;
+
+namespace UniPlanner.DAL
+{
+    public class LessonDAL
+    {
+        // CRUD
+        public IEnumerable<Lesson> GetLessons()
+        {
+            return null;
+        }
+    }
+}
